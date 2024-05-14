3eb2f56f86095dd30efc4a59bd55ca7c883184f5
Steven Hertz <StevenH@appi4.com>
2024-05-14 09:07:40 -0400

    Initial Commit

---
+++ Test doing lines/Test_doing_linesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_doing_linesApp.swift
+//  Test doing lines
+//
+//  Created by Steven Hertz on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_doing_linesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
